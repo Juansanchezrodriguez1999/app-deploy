dab83316704781e1afa848349b6f520e95fb26d6
juansanchezrodriguez1999 <juansanchezrodriguez1999@gmail.com>
2024-05-13 00:59:17 +0200

    add messages validation

--- schema/user.js
+++ schema/user.js
@@ -3,15 +3,14 @@ import * as yup from 'yup';
 const schema = yup
   .object()
   .shape({
-    email: yup.string().required().email('This field must be an e-mail address'),
-    username: yup.string().required().min(4),
-    fullname: yup.string().required().min(4),
-    password: yup.string().required().min(4),
+    email: yup.string().required('Email is required').email('This field must be an e-mail address'),
+    username: yup.string().required('Username is required').min(4),
+    fullname: yup.string().required('Fullname is required').min(4),
+    password: yup.string().required('Password is required').min(4),
     confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match'),
-    role: yup.string().oneOf(['user', 'admin']).default('user'),
+    role: yup.string().default('user'),
     createdAt: yup.date().default(() => new Date()),
     updatedAt: yup.date().default(() => new Date()),
-  })
-  .required();
+  });
 
 export default schema;
